8aa203b1ef461ea5b60128bcbee07e13b5e2348a
Tawfiq-moonHacker <54370347+Tawfiq-MoonHacker@users.noreply.github.com>
2021-12-25 11:30:22 +0200

    Update untitled5.py

--- untitled5.py
+++ untitled5.py
@@ -75,6 +75,13 @@ def add_subscription(address,date):
     greeter = w3.eth.contract(address = contract,abi = abi)
     greeter.functions.add_subscription(address,date).transact()
 
+def get_subscription(address):
+    w3 = Web3(Web3.HTTPProvider(metis_network))
+    w3.eth.default_account = main_address
+    
+    greeter = w3.eth.contract(address = contract,abi = abi)
+    greeter.functions.get_subscription(address).call()
+
 # result = add_video("sdfdf","0xAB36979F5353131de195464d213C76AaDFd5a3EC","date","nameadf","url1")
 
 result = getvideo("0xAB36979F5353131de195464d213C76AaDFd5a3EC",0)
